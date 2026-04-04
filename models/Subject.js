37d1db8638183f98da3bc4e34dd643ca368e1b85
Shakalen Shaikh <160219957+ShakalBhau0001@users.noreply.github.com>
2026-04-04 13:56:50 +0530

    Update Subject.js

--- models/Subject.js
+++ models/Subject.js
@@ -1,1 +1,23 @@
+const mongoose = require("mongoose");
 
+const subjectSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Subject name required"],
+      unique: true,
+      trim: true,
+    },
+    maxMarks: {
+      type: Number,
+      default: 100,
+    },
+    order: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Subject", subjectSchema);
