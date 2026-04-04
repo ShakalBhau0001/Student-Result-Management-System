bec7a888338e1454ecb7435281ef8096ba82687b
Shakalen Shaikh <160219957+ShakalBhau0001@users.noreply.github.com>
2026-04-04 13:55:39 +0530

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -1,1 +1,39 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
+const adminSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 6,
+      select: false, // never return password in queries
+    },
+    name: {
+      type: String,
+      default: "Admin",
+    },
+  },
+  { timestamps: true }
+);
+
+// Hash password before saving
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+// Compare password method
+adminSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model("Admin", adminSchema);
